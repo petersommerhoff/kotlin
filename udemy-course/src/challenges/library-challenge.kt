18de32c3c5d829d1a2e3bd1012b87f5134f484ae
petersommerhoff@gmail.com <petersommerhoff@gmail.com>
2017-08-14 13:37:16 +0200

    Remove unnecessary property overriding.

--- udemy-course/src/challenges/library-challenge.kt
+++ udemy-course/src/challenges/library-challenge.kt
@@ -12,10 +12,10 @@ interface Lendable {
 
 // The properties title, genre, and publicationYear can be included in the parent class because both books and DVDs
 // have these properties. The author and length properties however are included only in the child classes.
-abstract class InventoryItem(open val title: String,
-                             open val genre: String,
-                             open var publicationYear: Int,
-                             open var borrowed: Boolean) : Lendable {
+abstract class InventoryItem(val title: String,
+                             val genre: String,
+                             var publicationYear: Int,
+                             var borrowed: Boolean) : Lendable {
 
     override fun borrow() {
         if (!borrowed) {
@@ -38,10 +38,10 @@ abstract class InventoryItem(open val title: String,
 
 // A book will be not borrowed by default.
 // The class is called LibraryBook to prevent name clashes with previous challenges.
-class LibraryBook(override val title: String,
+class LibraryBook(title: String,
                   val author: String,
-                  override val genre: String,
-                  override var publicationYear: Int) : InventoryItem(title, genre, publicationYear, false) {
+                  genre: String,
+                  publicationYear: Int) : InventoryItem(title, genre, publicationYear, false) {
 
     fun read() {
         println("Reading a book by $author...")
@@ -49,17 +49,17 @@ class LibraryBook(override val title: String,
 
     override fun copy(): InventoryItem {
         val copy = LibraryBook(title, author, genre, publicationYear)
-        copy.borrowed = this.borrowed;
+        copy.borrowed = this.borrowed
 
         return copy
     }
 }
 
 // A DVD will also be not borrowed by default.
-class LibraryDVD(override val title: String,
-                 override val genre: String,
+class LibraryDVD(title: String,
+                 genre: String,
                  val length: Int,
-                 override var publicationYear: Int) : InventoryItem(title, genre, publicationYear, false) {
+                 publicationYear: Int) : InventoryItem(title, genre, publicationYear, false) {
 
     fun watch() {
         println("Watching $title...")
@@ -67,7 +67,7 @@ class LibraryDVD(override val title: String,
 
     override fun copy(): InventoryItem {
         val copy = LibraryDVD(title, genre, length, publicationYear)
-        copy.borrowed = this.borrowed;
+        copy.borrowed = this.borrowed
 
         return copy
     }
